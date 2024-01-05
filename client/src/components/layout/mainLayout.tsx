5702aac1b96c95fbbf0290d75d7fcd22c994cdbb
Amier Ali <amier_1998@hotmail.com>
2024-01-05 18:33:51 +0000

    feat: create context in outlet to store misdemeanours

--- client/src/components/layout/mainLayout.tsx
+++ client/src/components/layout/mainLayout.tsx
@@ -1,10 +1,19 @@
 import { Outlet } from "react-router-dom";
+import { createContext, useEffect, useState } from "react";
+import { Misdemeanour } from "../../types/misdemeanours.types";
+import useMisdeamnorContext from "../hooks/useContextMisdeamenour";
+
+export const MisdeamnorContext = createContext<Misdemeanour[]>([]);
 
 function MainLayout() {
+  const data = useMisdeamnorContext();
+
   return (
     <>
       <main>
-        <Outlet />
+        <MisdeamnorContext.Provider value={data || []}>
+          <Outlet />
+        </MisdeamnorContext.Provider>
       </main>
     </>
   );
