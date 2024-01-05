de1a862fcbdc4402d3e7d5a7f5c16937e3a5d19a
Amier Ali <amier_1998@hotmail.com>
2024-01-05 01:09:15 +0000

    feat: Add all components to confession page to display form

--- client/src/components/confession/confession.tsx
+++ client/src/components/confession/confession.tsx
@@ -1,6 +1,14 @@
+import { useState } from "react";
+import ConfessionDetails from "./confessionDetails";
+import ConfessionReasonForContact from "./confessionReasonContact";
+import ConfessionSubject from "./confessionSubject";
+
 const ConfessionPage: React.FC = () => {
+  const [subject, setSubject] = useState("");
+  const [reason, setReason] = useState("");
+  const [confessionDetails, setConfessionDetails] = useState("");
   return (
-    <>
+    <form className="form__container">
       <p>
         It's very difficult to catch people commiting misdemeanours so we
         appreciate it when citizens confess to us directly
@@ -9,7 +17,20 @@ const ConfessionPage: React.FC = () => {
         However, if you're just having a hard day and need to vent then you're
         welcome to contact us here too. Up to you!
       </p>
-    </>
+      <ConfessionSubject
+        confessionSubject={subject}
+        onChangeConfessionSubject={(value) => setSubject(value)}
+      />
+      <ConfessionReasonForContact
+        reasonForContact={reason}
+        onChangeReasonForContact={(value) => setReason(value)}
+      />
+      <ConfessionDetails
+        confessionDetails={confessionDetails}
+        onChangeConfessionDetail={(value) => setConfessionDetails(value)}
+      />
+      <button type="submit">confess</button>
+    </form>
   );
 };
 
