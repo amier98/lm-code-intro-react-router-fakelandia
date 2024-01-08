adf3bde4f216a0bddfb8444ea805fc71bd2acca7
Amier Ali <amier_1998@hotmail.com>
2024-01-08 01:36:35 +0000

    feat: finalise testing for misdemenour page

--- client/src/components/misdemeanour/misdeameanour.test.tsx
+++ client/src/components/misdemeanour/misdeameanour.test.tsx
@@ -13,43 +13,26 @@ afterAll(() => server.close());
 const server = setupServer();
 
 const props: MisdeamenaorsProps = {
-  citizenId: 2849,
+  citizenId: 2332,
   misdemeanour: "united",
   date: "07/01/2024",
 };
 describe("context for misdeamneour", () => {
-  //   it("GET misdeamnour list", async () => {
-  //     // server.use(
-  //     //   http.get("http://localhost:8080/api/misdemeanours/3", () => {
-  //     //     return new HttpResponse(
-  //     //       JSON.stringify({
-  //     //         citizenId: 6563,
-  //     //         date: "07/01/2024",
-  //     //         misdemeanour: "united",
-  //     //       }),
-  //     //       {
-  //     //         headers: {
-  //     //           "Content-Type": "application/json",
-  //     //         },
-  //     //       }
-  //     //     );
-  //     //   })
-  //     // );
-  //     render(<MisdeamnorItem {...props} />);
-
-  //     const text = await screen.findByText("07/01/2024");
-  //     expect(text).toBeInTheDocument();
-  //   });
   it("render misdemeanour on page", () => {
     render(<MisdeamnorItem {...props} />);
     const text = screen.getByText("😈united");
     expect(text).toBeInTheDocument();
   });
-  it("renders page when loaded", () => {
-    render(<Misdeamenaors />);
-    const text = screen.getByText("Citizen ID");
+
+  it("render citizen Id on page", () => {
+    render(<MisdeamnorItem {...props} />);
+    const text = screen.getByText("2332");
     expect(text).toBeInTheDocument();
-    const title2 = screen.getByText("Date");
-    expect(title2).toBeInTheDocument();
+  });
+
+  it("render misdeamnour date on page", () => {
+    render(<MisdeamnorItem {...props} />);
+    const text = screen.getByText("07/01/2024");
+    expect(text).toBeInTheDocument();
   });
 });
