48ad81fa27fe5564020a93abd736436c1238fcab
Amier Ali <amier_1998@hotmail.com>
2024-01-08 01:56:04 +0000

    feat: Remove underline in nav links

--- client/src/components/header/navHeader.tsx
+++ client/src/components/header/navHeader.tsx
@@ -4,16 +4,21 @@ const NavBarHeader: React.FC = () => {
   return (
     <>
       <nav className="nav__container">
-        {/* <DepartmentName /> */}
         <ul className="Nav__list">
           <li className="Nav__Links">
-            <NavLink to="/home">Home</NavLink>
+            <NavLink to="/home" style={{ textDecoration: "none" }}>
+              Home
+            </NavLink>
           </li>
           <li className="Nav__Links">
-            <NavLink to="/misdeameanors">Misdeameanours</NavLink>
+            <NavLink to="/misdeameanors" style={{ textDecoration: "none" }}>
+              Misdeameanours
+            </NavLink>
           </li>
           <li className="Nav__Links">
-            <NavLink to="/confession">Confess to us</NavLink>
+            <NavLink to="/confession" style={{ textDecoration: "none" }}>
+              Confess to us
+            </NavLink>
           </li>
         </ul>
       </nav>
